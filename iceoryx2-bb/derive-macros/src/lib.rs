865c68ef0ef528931773641214be2b0e713fc2a5
Jeff Ithier <jeff@ithier.dev>
2024-10-23 18:24:46 +0200

    Propagate errors from iceoryx2 via CXX API

--- iceoryx2-bb/derive-macros/src/lib.rs
+++ iceoryx2-bb/derive-macros/src/lib.rs
@@ -16,7 +16,7 @@ extern crate proc_macro;
 
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, Data, DeriveInput, Fields};
+use syn::{parse_macro_input, Data, DeriveInput, Expr, ExprLit, Fields, Lit};
 
 /// Implements the [`iceoryx2_bb_elementary::placement_default::PlacementDefault`] trait when all
 /// fields of the struct implement it.
@@ -94,6 +94,99 @@ pub fn placement_default_derive(input: TokenStream) -> TokenStream {
     let expanded = quote! {
         impl #impl_generics PlacementDefault for #name #ty_generics #where_clause {
             #place_default_impl
+        }
+    };
+
+    TokenStream::from(expanded)
+}
+
+#[proc_macro_derive(StaticStringRepresentation, attributes(StaticString))]
+pub fn as_static_string_derive(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    let name = &input.ident;
+    let (impl_generics, ty_generics, where_clause) = input.generics.split_for_impl();
+
+    let static_string_impl = match input.data {
+        Data::Enum(ref data_enum) => {
+            let match_arms = data_enum.variants.iter().map(|variant| {
+                let variant_ident = &variant.ident;
+
+                // Get the StaticString attribute if it exists
+                let static_string = variant
+                    .attrs
+                    .iter()
+                    .find_map(|attr| {
+                        if !attr.path().is_ident("StaticString") {
+                            return None;
+                        }
+
+                        match attr.meta.require_name_value() {
+                            Ok(meta) => {
+                                if let Expr::Lit(ExprLit {
+                                    lit: Lit::Str(lit), ..
+                                }) = &meta.value
+                                {
+                                    Some(lit.value())
+                                } else {
+                                    None
+                                }
+                            }
+                            _ => None,
+                        }
+                    })
+                    .unwrap_or_else(|| {
+                        // Default to converting variant name to snake_case with spaces
+                        let variant_str = variant_ident.to_string();
+                        variant_str
+                            .chars()
+                            .enumerate()
+                            .map(|(i, c)| {
+                                if i > 0 && c.is_uppercase() {
+                                    format!(" {}", c.to_lowercase())
+                                } else {
+                                    c.to_lowercase().to_string()
+                                }
+                            })
+                            .collect::<String>()
+                    });
+
+                match &variant.fields {
+                    Fields::Unit => {
+                        quote! {
+                            Self::#variant_ident => concat!(#static_string, "\0")
+                        }
+                    }
+                    Fields::Unnamed(_) => {
+                        quote! {
+                            Self::#variant_ident(..) => concat!(#static_string, "\0")
+                        }
+                    }
+                    Fields::Named(_) => {
+                        quote! {
+                            Self::#variant_ident{..} => concat!(#static_string, "\0")
+                        }
+                    }
+                }
+            });
+
+            quote! {
+                fn as_static_str(&self) -> &'static str {
+                    match self {
+                        #(#match_arms,)*
+                    }
+                }
+            }
+        }
+        _ => {
+            let err =
+                syn::Error::new_spanned(&input, "AsStaticString can only be derived for enums");
+            return err.to_compile_error().into();
+        }
+    };
+
+    let expanded = quote! {
+        impl #impl_generics AsStaticString for #name #ty_generics #where_clause {
+            #static_string_impl
         }
     };
 
