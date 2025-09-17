92d11d3a58fe07a62ddef9887fc5953228c4b4f9
Jeff Ithier <jeff@ithier.dev>
2025-09-17 17:24:22 +0200

    [#709] Organize CLI commands into separate files

--- iceoryx2-cli/iox2-config/src/command/generate.rs
+++ iceoryx2-cli/iox2-config/src/command/generate.rs
@@ -1,4 +1,4 @@
-// Copyright (c) 2024 Contributors to the Eclipse Foundation
+// Copyright (c) 2025 Contributors to the Eclipse Foundation
 //
 // See the NOTICE file(s) distributed with this work for additional
 // information regarding copyright ownership.
@@ -11,9 +11,7 @@
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
 use anyhow::Result;
-use colored::Colorize;
 use dialoguer::Confirm;
-use enum_iterator::all;
 use iceoryx2::config::Config;
 use iceoryx2_bb_posix::directory::Directory;
 use iceoryx2_bb_posix::file::Permission;
@@ -21,105 +19,8 @@ use iceoryx2_bb_posix::system_configuration::*;
 use iceoryx2_bb_posix::*;
 use iceoryx2_bb_system_types::file_path::FilePath;
 use iceoryx2_bb_system_types::path::Path;
-use std::panic::catch_unwind;
 
-/// Prints the whole system configuration with all limits, features and details to the console.
-pub fn print_system_configuration() {
-    println!(
-        "{}",
-        "posix system configuration".underline().bright_green()
-    );
-    println!();
-    println!(" {}", "system info".underline().bright_green());
-    all::<SystemInfo>().for_each(|i| {
-        println!(
-            "  {:<50} {}",
-            format!("{i:?}").white(),
-            format!("{}", i.value()).bright_blue(),
-        );
-    });
-
-    println!();
-    println!(" {}", "limit".underline().bright_green());
-    for i in all::<Limit>().collect::<Vec<_>>() {
-        let limit = i.value();
-        let limit = if limit == 0 {
-            "[ unlimited ]".to_string()
-        } else {
-            limit.to_string()
-        };
-        println!("  {:<50} {}", format!("{i:?}").white(), limit.bright_blue(),);
-    }
-
-    println!();
-    println!(" {}", "options".underline().bright_green());
-    for i in all::<SysOption>().collect::<Vec<_>>() {
-        if i.is_available() {
-            println!(
-                "  {:<50} {}",
-                format!("{i:?}").white(),
-                format!("{}", i.is_available()).bright_blue()
-            );
-        } else {
-            println!("  {:<50} {}", format!("{:?}", i), i.is_available(),);
-        }
-    }
-
-    println!();
-    println!(" {}", "features".underline().bright_green());
-    for i in all::<Feature>().collect::<Vec<_>>() {
-        if i.is_available() {
-            println!(
-                "  {:<50} {}",
-                format!("{i:?}").white(),
-                format!("{}", i.is_available()).bright_blue(),
-            );
-        } else {
-            println!("  {:<50} {}", format!("{:?}", i), i.is_available(),);
-        }
-    }
-
-    println!();
-    println!(" {}", "process resource limits".underline().bright_green());
-    for i in all::<ProcessResourceLimit>().collect::<Vec<_>>() {
-        let soft_limit_result = catch_unwind(|| i.soft_limit());
-        let hard_limit_result = catch_unwind(|| i.hard_limit());
-
-        match (soft_limit_result, hard_limit_result) {
-            (Ok(soft), Ok(hard)) => {
-                println!(
-                    "  {:<43} soft:  {:<24} hard:  {}",
-                    format!("{i:?}").white(),
-                    format!("{soft}").bright_blue(),
-                    format!("{hard}").bright_blue()
-                );
-            }
-            (Err(e), _) | (_, Err(e)) => {
-                println!(
-                    "  {:<43} Error: {}",
-                    format!("{i:?}").white(),
-                    format!("Unable to acquire limit due to: {e:?}").red()
-                );
-            }
-        }
-    }
-}
-
-pub fn show_system_config() -> Result<()> {
-    print_system_configuration();
-
-    Ok(())
-}
-
-pub fn show_current_config() -> Result<()> {
-    let config = Config::global_config();
-    let toml_config = toml::to_string_pretty(&config)?;
-    println!("{toml_config}");
-
-    Ok(())
-}
-
-pub fn generate_global() -> Result<()> {
+pub(crate) fn generate_global() -> Result<()> {
     let mut global_config_path = get_global_config_path();
     global_config_path.add_path_entry(&iceoryx2::config::Config::relative_config_path())?;
     let filepath = FilePath::from_path_and_file(
@@ -131,7 +32,7 @@ pub fn generate_global() -> Result<()> {
     generate(global_config_path, filepath)
 }
 
-pub fn generate_local() -> Result<()> {
+pub(crate) fn generate_local() -> Result<()> {
     let user = iceoryx2_bb_posix::user::User::from_self().unwrap();
     let mut user_config_path = match user.details() {
         Some(details) => details.config_dir().clone(),
